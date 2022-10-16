1d3789b9bd11f858cdc3336ed4a5242dbd57a1de
mrtechnostart <rambpandey238@gmail.com>
2022-10-16 21:33:38 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,91 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from docx import Document
+def downloadNow_faculty(data):
+    file = Document()
+    file.add_paragraph("{},".format(data["receiver"]))
+    file.add_paragraph("Date:{}".format(data["date3"]))
+    file.add_paragraph("Subject:{}".format(data["subject"]))
+    file.add_paragraph("Sir/Ma'am")
+    no_of_day = data["no_of_day"]
+    starting = data["starting_date"]
+    end = data["end_date"]
+    reason = data["reason"]
+    resume = data["resume_date"]
+    person_name = data["person_name"]
+    Name = data["Name"]
+    file.add_paragraph("I am writing to ask you for a {} days leave from {} to {}  due to {} . I am going to resume work from the {}.".format(no_of_day,starting,end,reason,resume))
+    file.add_paragraph("I shall be reachable on my mobile number and email during the period. My person in charge, {} will be handling my tasks in my absence.".format(person_name))
+    file.add_paragraph("""I will be thankful to you for considering my application.
+    
+    
+Yours Sincerely,
+    
+{}""".format(Name))
+
+    file.save("generatedLetter.docx")
+    with open("generatedLetter.docx", "rb") as file:
+        btn = st.download_button(
+            label="Download Now",
+            data=file,
+            file_name="Generated_Resume.docx",
+            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+          )
+def home_faculty():
+    name= st.text_input("Name")
+    receiver = st.text_area("Receiver Details")
+    subject = st.text_input("Enter your subject")
+    date1 = str(st.date_input("Enter the starting date"))
+    date2 = str(st.date_input("Enter the last date date"))
+    no_of_day = st.text_input("enter the no of days")
+    date3 = str(st.date_input("Enter the date of submission of the application"))
+    reason = st.text_area("Enter The Reason ")
+    resume_date = str(st.date_input("Enter the returning date"))
+    person_name = st.text_input("Enter the name of the person who can represent you, during leave")
+    if st.button("Submit"):
+        datadict = {
+        "Name" : name,
+        "starting_date" : date1,
+        "end_date" : date2,
+        "no_of_day": no_of_day,
+        "receiver" : receiver,
+        "subject" : subject,
+        "date3" : date3,
+        "reason" : reason,
+        "resume_date" :  resume_date,
+        "person_name" : person_name
+    }
+        downloadNow_faculty(datadict)
+def letter():
+    st.title("Formal Application Generator")
+    st.subheader("Dashboard")
+    selected = option_menu(
+        menu_title="Main Menu",
+        options=["Home","Leave Application","About","Contact Us"],
+        icons=["house","book","envelop","contact"],
+        menu_icon="cast",
+        orientation="horizontal"
+        )
+    if selected=="Home":
+        st.markdown("Hello EveryOne")
+    if selected=="Leave Application":
+        home_faculty()
+    if selected == "Contact Us":
+        st.markdown("""
+        ## YOU CAN CONTACT ME -
+        Instagram : mrtechnostart                        
+        Email Id : rambpandey238@gmail.com
+        """)
+        st.text("IMS Engineering College, IMS Hostel, Ghaziabad Varanasi")
+        st.markdown("""
+        
+        ## Team Members :-
+        1. Ram Badan Pandey
+        2. Rohit Kumar Pandey
+        3. Saif Mohammad
+        4. Subodh Dubey
+        5. Suryansh Katiyar
+        """)
+    if selected == "About":
+        st.markdown("We Are Here To ")
+letter()
